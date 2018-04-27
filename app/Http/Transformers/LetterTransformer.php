397119ab8221c7629e22a765a16457deb32b45c9
frank <yangjunalns@qq.com>
2018-04-27 23:42:13 +0800

    transform修改

--- app/Http/Transformers/LetterTransformer.php
+++ app/Http/Transformers/LetterTransformer.php
@@ -24,7 +24,10 @@ class LetterTransformer extends TransformerAbstract
           'is_public' => $letter['is_public'],
           'like_count' => $letter['like_count'],
           'is_like' => $letter['is_like'],
-          'comment_count' => $letter['comment_count']
+          'comment_count' => $letter['comment_count'],
+          'create_date' => date('Y年m月d日', strtotime($letter['created_at'])),
+          'arrive_date' => date('Y年m月d日', strtotime($letter['arrive_time'])),
+          'create_time' => $letter['created_at']
         ];
     }
 
