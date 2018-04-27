f99e228fb39e300f68277117facf4ef8319203e0
frank <yangjunalns@qq.com>
2018-04-28 00:20:38 +0800

    日期修改

--- app/Http/Transformers/LetterTransformer.php
+++ app/Http/Transformers/LetterTransformer.php
@@ -14,9 +14,19 @@ class LetterTransformer extends TransformerAbstract
 
     public function transform(Letter $letter)
     {
+        $content = $letter['content'];
+        $description = mb_strlen($content) > 60 ? mb_substr($content, 0, 60, 'utf-8').'...' : $content;
+        $arrive_time_stemp = strtotime($letter['arrive_time']);
+        $created_at_stemp = strtotime($letter['created_at']);
+
+        $diff = $this->timediff($arrive_time_stemp, $created_at_stemp);
+        $meta = '寄给'.$diff.'后';
+
         return [
           'id'   => $letter['id'],
           'title'   => $letter['title'],
+          'meta'   => $meta,
+          'description' => $description,
           'content' => $letter['content'],
           'images' => $letter['images'],
           'arrive_time' => $letter['arrive_time'],
@@ -25,8 +35,8 @@ class LetterTransformer extends TransformerAbstract
           'like_count' => $letter['like_count'],
           'is_like' => $letter['is_like'],
           'comment_count' => $letter['comment_count'],
-          'create_date' => date('Y年m月d日', strtotime($letter['created_at'])),
-          'arrive_date' => date('Y年m月d日', strtotime($letter['arrive_time'])),
+          'create_date' => date('Y-m-d', $created_at_stemp),
+          'arrive_date' => date('Y-m-d', $arrive_time_stemp),
           'create_time' => $letter['created_at']
         ];
     }
@@ -34,5 +44,21 @@ class LetterTransformer extends TransformerAbstract
     public function includeWxUser(Letter $letter)
     {
         return $this->item($letter->wxuser, App::make(WxUserTransformer::class));
+    }
+
+    function timediff( $begin_time, $end_time )
+    {
+      if ( $begin_time < $end_time ) {
+        $starttime = $begin_time;
+        $endtime = $end_time;
+      } else {
+        $starttime = $end_time;
+        $endtime = $begin_time;
+      }
+      $timediff = $endtime - $starttime;
+      $years = intval( $timediff / 31536000 );
+      $months = intval( $timediff / 2628000 );
+
+      return $years != 0 ? $years.'年' : $months.'月';
     }
 }
